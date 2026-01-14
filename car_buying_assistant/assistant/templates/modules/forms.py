d3b186f513aea865849f74adfcb4237c3132670a
Michał Walczak <151867988+michalspoko18@users.noreply.github.com>
2026-01-14 21:03:36 +0100

    Add dependent model and specification fields

--- car_buying_assistant/assistant/templates/modules/forms.py
+++ car_buying_assistant/assistant/templates/modules/forms.py
@@ -1,6 +1,34 @@
 from django import forms
 
+from assistant.models import CarMake, CarModel, CarSpecification
+
 
 class ChooseCar(forms.Form):
-    brand = forms.CharField(label="Brand", max_length=100)
-    model = forms.CharField(label="Model", max_length=100)
+    brand = forms.ChoiceField(label="Brand", choices=[])
+    model = forms.ChoiceField(label="Model", choices=[], required=False)
+    specification = forms.ModelChoiceField(
+        label="Specification",
+        queryset=CarSpecification.objects.using("car2db").all(),
+        required=False,
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["brand"].choices = self._brand_choices()
+        self.fields["model"].choices = self._model_choices()
+
+    @staticmethod
+    def _brand_choices():
+        brands = CarMake.objects.using("car2db").all()
+        return [("", "Select brand")] + [
+            (brand.id_car_make, brand.name) for brand in brands
+        ]
+
+    def _model_choices(self):
+        selected_brand = self.data.get("brand") or self.initial.get("brand")
+        if not selected_brand:
+            return [("", "Select model")]
+        models = CarModel.objects.using("car2db").filter(id_car_make=selected_brand)
+        return [("", "Select model")] + [
+            (model.id_car_model, model.name) for model in models
+        ]
